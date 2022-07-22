1d5d1f46dd43bf3942abfae66408797d68f41fba
C-Joys <chebetjoyce97@gmail.com>
2022-07-22 06:18:43 -0700

    multiply 2numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - multiplies 2 numbers and prints the results.
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 if it works, 1 if there is an error
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, m, diff;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	diff = n * m;
+
+	printf("%i\n", diff);
+
+	return (0);
+}
